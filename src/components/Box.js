d03c5e21d0951cbe30f86f96cd7db471ddff16c3
FrancineDash <54555191+FrancineDash@users.noreply.github.com>
2020-10-04 23:34:34 -0400

    Initial Commit

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,32 +1,16 @@
 import React from 'react';
-//import {Link} from "react-router-dom";
+
 
 function Box(props){
 
-   // const [name]=React.useState(props.name);
-    const [number]=React.useState(props.age);
-    //const [color]=React.useState(props.color);
-    //const [id]=React.useState(props.id)
-
-    //const addUp = ()=>{
-     //  setNumber(number+10);
-     //  if(number>=80){
-     //      setColor("#E0FFFF")
-      //   }
-     //};
-
-    let style ={
-        backgroundColor:"none",
-        minheight: number+"px",
-        color:"white"
-    };
+    const [id]=React.useState(props.name);
+    const [color] = React.useState (props.color);
 
     return(
-        <div className={"box"} style={style}>
-            <div style={{backgroundColor:"green", height:"100px", width:"100px", marginTop:"30px"}}></div>
-        </div>
+
+        <div style={{backgroundColor:color, width:"100",height:"100px", marginTop:"30px"}}>
+        {id}</div>
     )
 }
 
 export default Box;
-
--- src/components/DataPage.js
+++ src/components/DataPage.js
@@ -1,46 +1,62 @@
-import React from 'react';
+import React from "react";
 import Box from "./Box";
 
-function DataPage(props){
-    const[number, setNumber]=React.useState(0);
-    const[color,setColor]=React.useState("purple");
-    const[data,setData]=React.useState([{name:"Luke", age:26}]);
-    const[submit]=React.useState(false);
-    const[oddNumber, setOddNumber]=React.useState(1)
-    //const[reset]=React.useState(0);
+
+function DataPage (props){
+
+    const [count, setCount] = React.useState(0);
+    const [color, setColor] = React.useState ("purple");
+    const [data, setData] = React.useState([{id: ""}]);
+    const [submit, setSubmit] = React.useState(false);
+
+    const colors = ["blue","green","orange"];
+    const randColor = colors[Math.floor(Math.random() * colors.length)];
 
     React.useEffect(()=>{
-        if(number>5){
-            setColor("red");
+        if (count === 0){
+            setColor("purple")
+        }
+        else if(count % 5 ===0) {
+            setColor(randColor)
+        }
+        else{
+            setColor("purple")
         }
 
-    }, [number]);
-
+    }, [color, randColor, count]);
 
     React.useEffect(()=>{
-
         let ogData = data;
-        ogData.push({name:"freddy", age:30});
+        ogData.push({id: "_______________"});
         setData(ogData);
+    }, [data, submit]);
 
-    }, [number,data, submit]);
 
     let boxEles = data.map((it, idx)=>
-        <Box name={it.name} age={it.age}/>
-
+        <Box name={it.id} idx={idx.id}/>
     )
+    function reset() {
+        window.location.reload(false);
+    }
+    function addUp(){
+        setCount  (count + 1);
+        if (count % 2 !== 0){
+            setSubmit(!submit);
+        }
+        else{
+            setSubmit(submit);
+        }
+    }
 
-    return(
+    return (
         <div>
             <h1>{props.match.params.id}</h1>
-            <button onClick={()=>setNumber(number+1)&&"odd"(data+1)}>Increase by 1</button>
-            <button onClick={()=>setNumber(0)&&setOddNumber(oddNumber===0)}>Reset</button>
-            <h3>{number}</h3>
-            <div style={{backgroundColor:color, height:"100px", width:"100px", marginTop:"30px"}}></div>
-            {boxEles}
-
+            <button onClick={addUp}>Increase by 1</button>
+            <button onClick={reset}>Reset</button>
+            <h3>{count}</h3>
+            <div style={{backgroundColor:color, height: "100",width:"100px",marginTop:"30px"}}>
+                {boxEles}</div>
         </div>
     )
 }
-
-export default DataPage;
+export default DataPage
